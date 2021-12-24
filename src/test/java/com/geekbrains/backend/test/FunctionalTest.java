70f95743373cdd76862125a3059a1ca36ff394d2
oxans <54911869+oxans@users.noreply.github.com>
2021-12-24 09:06:16 +0300

    Update FunctionalTest.java

--- src/test/java/com/geekbrains/backend/test/FunctionalTest.java
+++ src/test/java/com/geekbrains/backend/test/FunctionalTest.java
@@ -2,10 +2,12 @@ package com.geekbrains.backend.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public abstract class FunctionalTest {
@@ -13,7 +15,7 @@ public abstract class FunctionalTest {
 
     public static Properties readProperties() throws Exception {
         Properties properties = new Properties();
-        Files.readAllLines(Path.of(
+        Files.readAllLines(Paths.get(
                         FunctionalTest.class.getResource("test.properties").toURI()))
                 .forEach(str -> {
                     String[] props = str.split("=");
@@ -24,9 +26,13 @@ public abstract class FunctionalTest {
 
     public String getStringResource(String name) throws IOException {
         String dir = getClass().getSimpleName();
-        byte[] bytes = getClass().getResourceAsStream(dir + "/" + name)
-                .readAllBytes();
-        return new String(bytes, StandardCharsets.UTF_8);
+        InputStream is = getClass().getResource(dir + "/" + name)
+                .openStream();
+        StringBuilder s = new StringBuilder();
+        while (is.available() > 0) {
+            s.append((char) is.read());
+        }
+        return s.toString();
     }
 
     public File getFileResource(String name) {
